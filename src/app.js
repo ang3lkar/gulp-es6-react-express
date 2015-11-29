24454a9f0f777618dd4e5210ed2206d5c7b1777d
Angelos Karagkiozidis <angelkar@gmail.com>
2015-11-29 17:36:55 +0200

    Initial commit

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ class App extends React.Component {
   render() {
     return (
       <main>
-        You have been spooked
+        You have been spooked!
       </main>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('container'))
